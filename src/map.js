1a40a0338439c6f657e362a57378784e10f16822
mclick <mclick@ucsc.edu>
2021-09-09 14:24:46 -0700

    map works better

--- src/map.js
+++ src/map.js
@@ -1,12 +1,12 @@
 class Map {
 
-
+    
 
     getPixPos(x,y){
         return [x*16, y*16];
     }
 
     getGridPos(x, y){
-        return [int(x/16),int(y/16)];
+        return [Math.Trunc(x/16),Math.Trunc(y/16)];
     }
 }
